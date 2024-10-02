20490669b12576b6ca44d43cf0e665080a243c52
Rasmus Stavenuiter <rasmus.k.s@proton.me>
2024-10-02 16:20:19 +0200

    Add some documentation

--- src/algorithms/delaunay/triangulation.hpp
+++ src/algorithms/delaunay/triangulation.hpp
@@ -30,23 +30,39 @@ struct DelaunayTriangulator
     DelaunayTriangulator() = default;
     DelaunayTriangulator(std::vector<primitives::Point> points) : m_vertices(points) {}
 
+    /// @brief Performs a Delaunay triangulation of the current set of vertices.
+    ///        If the triangulation already has edges, they are cleared first.
+    ///        If the triangulation fails before finishing, edges are cleared and <searchHierarchy>
+    ///        is set to nullopt so as to not leave the triangulator in an invalid state.
+    /// @return Whether the triangulation was successful.
     bool performTriangulation();
 
+    /// @brief Checks if all edges are Delaunay by checking whether the triangles formed by their opposing
+    ///        vertices have circumcircles that contain no other vertices.
+    /// @return Whether all edges are Delaunay or not.
     bool isDelaunay() const;
+
+    /// @brief Legalizes edges in the triangulation, and also "recursively" (though not actually implemented
+    /// as a recursive function) legalizes edges that are created as a result of any edge flips on the way.
+    /// @param legalizationCandidates Edge candidates to consider first. If empty, all edges are considered.
+    /// @return The number of edge flips made during the legalization process.
     int legalizeEdges(std::vector<Edge> legalizationCandidates = {});
 
     const std::vector<primitives::Point>& getVertices() const { return m_vertices; };
     const std::multimap<size_t, size_t>& getEdges() const { return m_edges; };
     
 protected:
+    std::vector<primitives::Point> m_vertices;
+    std::multimap<size_t, size_t> m_edges;
+
     // Utility stuff
     std::pair<size_t, std::optional<size_t>> getOpposingVerticesToEdge(Edge edge, bool throwOnDegenerateTris = true) const;
     void addEdge(size_t v1, size_t v2, bool legalizeAfterInsertion = true);
+    // NB: throws if called on exterior edge, i. e., edge, which does not have two opposing vertices.
     Edge flipEdge(Edge edge);
-    std::optional<TriangleSearchHierarchy> searchHierarchy;
-
-    std::vector<primitives::Point> m_vertices;
-    std::multimap<size_t, size_t> m_edges;
+    // Only added as a data member to not have to pass it around as a parameter to every function.
+    // Should be set to nullopt whenever <performTriangulation> is not running.
+    std::optional<TriangleSearchHierarchy> searchHierarchy = std::nullopt;
 };
 
 } // namespace algorithms
